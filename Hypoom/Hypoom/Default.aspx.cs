08eb40fdc3569caace81d7b7dc356f4935693697
Niek van Dam <niekvandam1@outlook.com>
2019-11-18 10:18:21 +0100

    Cleaning up the template mess

--- Hypoom/Hypoom/Default.aspx.cs
+++ Hypoom/Hypoom/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,11 @@ namespace Hypoom
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        protected void clickButton_OnClick(object sender, EventArgs e)
+        {
+            
         }
     }
 }
